f110c3529f2c18038d2c2a22bd0f987c28c59e8a
Atanu Biswas <103525450+Atanub707@users.noreply.github.com>
2024-05-23 17:13:20 +0530

    Update Docker-Test-SetUp.sh

--- Docker-Test-SetUp.sh
+++ Docker-Test-SetUp.sh
@@ -11,7 +11,7 @@ sudo apt-get install ca-certificates curl gnupg git -y
 
 # Add Docker's official GPG key
 echo "Adding Docker GPG key..."
-sudo install -m 0755 -d /etc/apt/keyrings
+yes | sudo cp -f /dev/null /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
